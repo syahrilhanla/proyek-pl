642bb512206784577213c2e82e3ed960521a02e1
syahrilhanla <syahrilhanla5@gmail.com>
2020-12-21 23:39:06 +0800

    Tambah footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () => {
+	return (
+		<div className='footer'>
+			<span>@Copyright Syahril 2020</span>
+		</div>
+	);
+};
--- client/src/components/pages/HomeAdmin.js
+++ client/src/components/pages/HomeAdmin.js
@@ -3,6 +3,7 @@ import { Redirect } from "react-router-dom";
 import { TimeLineCardNew } from "../TimeLineCardNew";
 import { GlobalContext } from "../globalState/GlobalState";
 import { Navbar } from "../Navbar";
+import { Footer } from "../Footer";
 
 // Check if logged in
 export const checkLogin = (loginInfo, history) => {
@@ -37,11 +38,20 @@ export const HomeAdmin = () => {
 			<>
 				<div className='container'>
 					<label>
-						<h1 style={{ borderBottom: "2px solid #b8bdb5" }}>Lini Masa</h1>
+						<h1
+							style={{
+								borderBottom: "2px solid #b8bdb5",
+								marginTop: "-20px",
+							}}
+						>
+							Lini Masa
+						</h1>
 					</label>
-					{borrowingList.map((list) => (
-						<TimeLineCardNew key={list._id} borrowingList={list} />
-					))}
+					<div style={{ height: "490px", overflow: "auto" }}>
+						{borrowingList.map((list) => (
+							<TimeLineCardNew key={list._id} borrowingList={list} />
+						))}
+					</div>
 				</div>
 			</>
 		);
@@ -51,6 +61,7 @@ export const HomeAdmin = () => {
 		<>
 			<Navbar user={"adm"} invisible={invisible} setInvisible={setInvisible} />
 			<>{checkLogin(loginInfo) ? <Home /> : <Redirect to='/' />}</>
+			<Footer />
 		</>
 	);
 };
